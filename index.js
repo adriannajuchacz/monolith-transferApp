badf24d9ad9d781e94cc6ceec556cac5ad8cc567
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-06-17 18:42:57 +0200

    added POST /transferMoney

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
 const app = express();
+/* mocked DB balance */ 
+let balance = 0;
+
+/* ENDPOINTS START */
+app.post('/transferMoney', (req, res) => {
+    balance += 100;
+    res.status(200).send(`current balance: ${balance}`);
+});
+/* ENDPOINTS END */
 
 const PORT = process.env.PORT || 5000;
 
